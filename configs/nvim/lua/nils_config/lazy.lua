68b07eb3164e0fc9f73543b6d450d54cc98e6d1e
Nils Herzig <git@nilsherzig.com>
2024-09-01 21:45:01 +0200

    removed markview from neovim

--- configs/nvim/lua/nils_config/lazy.lua
+++ configs/nvim/lua/nils_config/lazy.lua
@@ -170,20 +170,20 @@ require("lazy").setup({
 			-- end
 		end,
 	},
-	{
-		"OXY2DEV/markview.nvim",
-		lazy = false, -- Recommended
-		-- ft = "markdown" -- If you decide to lazy-load anyway
-
-		dependencies = {
-			-- You will not need this if you installed the
-			-- parsers manually
-			-- Or if the parsers are in your $RUNTIMEPATH
-			"nvim-treesitter/nvim-treesitter",
-
-			"nvim-tree/nvim-web-devicons",
-		},
-	},
+	-- {
+	-- 	"OXY2DEV/markview.nvim",
+	-- 	lazy = false, -- Recommended
+	-- 	-- ft = "markdown" -- If you decide to lazy-load anyway
+	--
+	-- 	dependencies = {
+	-- 		-- You will not need this if you installed the
+	-- 		-- parsers manually
+	-- 		-- Or if the parsers are in your $RUNTIMEPATH
+	-- 		"nvim-treesitter/nvim-treesitter",
+	--
+	-- 		"nvim-tree/nvim-web-devicons",
+	-- 	},
+	-- },
 	{
 		"barreiroleo/ltex_extra.nvim",
 		branch = "dev",
